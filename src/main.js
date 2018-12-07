50408d1bcd2dbe3fdef1933d6610de1c102476a9
Milena <milenacuk89@gmail.com>
2018-12-07 19:04:40 +0100

    Zadatak 2

--- src/main.js
+++ src/main.js
@@ -2,14 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import TriviaApp from './components/TriviaApp.vue'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
+Vue.use(Vuex)
 
 const routes = [
   { path: '/', redirect: '/triviaApp'},
   { path: '/triviaApp', component: TriviaApp}
 ]
+const store = new Vuex.Store({
+  modules: {
+    ChuckModule,
+    TriviaModule
+  }
+})
+
 const router = new VueRouter({
   mode: 'history',
   routes
@@ -17,5 +26,6 @@ const router = new VueRouter({
 new Vue({
   
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')

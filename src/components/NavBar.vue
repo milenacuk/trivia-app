e82e659a33472ecda24fddd350240407a63be11a
Milena <milenacuk89@gmail.com>
2018-12-07 18:44:19 +0100

    Zadatak 1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="navbar navbar-dark bg-primary">
+
+        <a class="navbar-brand" href="#">
+            <img src="/docs/4.1/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+             TriviaApp
+        </a>
+    </nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import TriviaApp from './components/TriviaApp.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 
+const routes = [
+  { path: '/', redirect: '/triviaApp'},
+  { path: '/triviaApp', component: TriviaApp}
+]
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 new Vue({
+  
+  router,
   render: h => h(App),
 }).$mount('#app')
